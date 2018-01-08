dda142a18791f951ad7e788f0a7fc7e2e2819662
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-01-08 15:12:39 -0500

    update

--- OrcMathGUI/src/simon/ProgressAchilles.java
+++ OrcMathGUI/src/simon/ProgressAchilles.java
@@ -6,20 +6,24 @@ import guiTeacher.components.Component;
 
 public class ProgressAchilles extends Component implements ProgressInterfaceAchilles{
 
+	private boolean isGameOver = false;
+	
 	public ProgressAchilles(int x, int y, int w, int h) {
 		super(x, y, w, h);
 	}
 
 	@Override
 	public void gameOver() {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 	@Override
 	public void update(Graphics2D g) {
-		// TODO Auto-generated method stub
-		
+		if(isGameOver) {
+			gameOver();
+		}else {
+			
+		}
 	}
 
 	@Override
